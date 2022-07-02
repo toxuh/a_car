821297e3566f2480626381e9f0580e17f88fcb6a
Anton <zakharov.toxa@gmail.com>
2022-07-02 18:04:50 +0200

    car moves forward and reverse with some physics

--- src/Car.ts
+++ src/Car.ts
@@ -6,6 +6,12 @@ export class Car {
   w: number;
   h: number;
 
+  speed: number;
+  maxSpeed: number;
+  reverseSpeedRatio: number;
+  acceleration: number;
+  friction: number;
+
   controls: Controls;
 
   constructor(x: number, y: number, w: number, h: number) {
@@ -14,17 +20,41 @@ export class Car {
     this.w = w;
     this.h = h;
 
+    this.speed = 0;
+    this.maxSpeed = 3;
+    this.reverseSpeedRatio = 0.5;
+    this.acceleration = 0.2;
+    this.friction = 0.05;
+
     this.controls = new Controls();
   }
 
   update() {
     if (this.controls.forward) {
-      this.y -= 2;
+      this.speed += this.acceleration;
     }
 
     if (this.controls.reverse) {
-      this.y += 2;
+      this.speed -= this.acceleration;
     }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+
+    if (this.speed < -this.maxSpeed * this.reverseSpeedRatio) {
+      this.speed = -this.maxSpeed * this.reverseSpeedRatio;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    this.y -= this.speed;
   }
 
   draw(ctx: CanvasRenderingContext2D | null) {
